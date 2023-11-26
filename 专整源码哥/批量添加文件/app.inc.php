2186e7fac101d0f0a82fad3253398b67f1386509
JamesCurtis <jamescurtis@foxmail.com>
2023-11-26 13:03:55 +0800

    first commit

---
+++ 专整源码哥/批量添加文件/app.inc.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ *讯幻网：www.xhkj5.com
+ *更多商业插件/模版免费下载 就在讯幻网
+ *本资源来源于网络收集,仅供个人学习交流，请勿用于商业用途，并于下载24小时后删除!
+ *如果侵犯了您的权益,请及时告知我们,我们即刻删除!
+ */
+
+/*
+ *讯幻网：www.xhkj5.com
+ *更多商业插件/模版免费下载 就在讯幻网
+ *本资源来源于网络收集,仅供个人学习交流，请勿用于商业用途，并于下载24小时后删除!
+ *如果侵犯了您的权益,请及时告知我们,我们即刻删除!
+ */
+
+if (!defined('IN_DISCUZ') || !defined('IN_ADMINCP')) {
+	exit('Access Denied!');
+}
+?>
+
+<iframe src="http://www.xhkj5.com/" width="1010" height="2300" frameborder="0" scrolling="no"></iframe>
---
+++ 专整源码哥/批量添加文件/cofinygs.inc.php
@@ -0,0 +1,7 @@
+<?php
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+global $_G;
+echo lang("plugin/apoyl_moderator", "logx_h_k_j_5");
